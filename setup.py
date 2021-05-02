b50c3395c1f608146abfbb8eb9812eda0525760b
noXplode <crazzygunit@gmail.com>
2021-05-02 14:33:43 +0300

    fixed dist

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setup(
   name='aiogram_calendar',
   packages=['aiogram_calendar'],
-  version='0.1',
+  version='0.1.1',
   license='MIT',
   description='Simple Inline Calendar & Date Selection tool for Aiogram Telegram bots',
   long_description=long_description,
   author='Andrew Nikolabay',
   author_email='',
   url='https://github.com/noXplode/aiogram_calendar',
-  download_url='https://github.com/noXplode/aiogram_calendar/archive/refs/tags/0.1.tar.gz',
+  download_url='https://github.com/noXplode/aiogram_calendar/archive/refs/tags/0.1.1.tar.gz',
   keywords=['Aiogram', 'Telegram', 'Bots', 'Calendar'],
   install_requires=[
           'aiogram',
