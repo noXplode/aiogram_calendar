266fe58d751cde121e5284df461c6de264f50843
Murphy <thehelixpg@gmail.com>
2022-12-05 18:05:16 +0200

    sdist package ready, deployed to pypi

--- aiogram3_calendar/__init__.py
+++ aiogram3_calendar/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 import aiogram
-assert aiogram.__version__.split('.')[0] == 3, 'Current module requires aiogram package version 3.x.x'
+assert aiogram.__version__.split('.')[0] == '3', 'Current module requires aiogram package version 3.x.x'
 
 from aiogram3_calendar.simple_calendar import SimpleCalendarCallback as simple_cal_callback, SimpleCalendar
 from aiogram3_calendar.dialog_calendar import DialogCalendarCallback as dialog_cal_callback, DialogCalendar
--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name='aiogram_calendar',
-  packages=['aiogram_calendar'],
+  name='aiogram3_calendar',
+  packages=['aiogram3_calendar'],
   version='0.1.2b',
   license='MIT',
-  description='Simple Inline Calendar & Date Selection tool for Aiogram Telegram bots',
+  description='Simple Inline Calendar & Date Selection tool for Aiogram (version 3.0.0a12 and upper) Telegram bots',
   long_description=long_description,
   author='Andrew Nikolabay, Dmytro Yaroshenko',
   author_email='',
   url='https://github.com/o-murphy/aiogram3_calendar',
   keywords=['Aiogram', 'Aiogram3' 'Telegram', 'Bots', 'Calendar'],
   install_requires=[
-          'aiogram>=3.0.0a12',
+          'aiogram~=3.0.0b6',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
