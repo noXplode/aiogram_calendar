a4338d6a6e13ccd2d5d5144523effb7b23a40dae
barabum0 <r.m.poltorabatko@gmail.com>
2023-06-17 13:40:35 +0300

    updated dialog calendar to aiogram3

--- aiogram_calendar/dialog_calendar.py
+++ aiogram_calendar/dialog_calendar.py
@@ -1,14 +1,20 @@
 import calendar
 from datetime import datetime
 
+from aiogram import F
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.callback_data import CallbackData
 from aiogram.types import CallbackQuery
 
 
 # setting callback_data prefix and parts
-calendar_callback = CallbackData('dialog_calendar', 'act', 'year', 'month', 'day')
-ignore_callback = calendar_callback.new("IGNORE", -1, -1, -1)  # for buttons with no answer
+dialog_callback_filter = F.data.startswith('dialog_calendar')
+
+
+def build_data(action: str, year: int, month: int, day: int): return f"dialog_calendar&{action}&{year}&{month}&{day}"
+def process_data(data: str) -> dict: data = data.split("&")[1:]; return {"act": data[0], "year": data[1], "month": data[2], "day": data[3]}
+
+
+ignore_callback = build_data("IGNORE", -1, -1, -1)
 
 
 class DialogCalendar:
@@ -22,95 +28,104 @@ class DialogCalendar:
         self,
         year: int = datetime.now().year
     ) -> InlineKeyboardMarkup:
-        inline_kb = InlineKeyboardMarkup(row_width=5)
+        inline_kb = []
         # first row - years
-        inline_kb.row()
+        inline_row = []
         for value in range(year - 2, year + 3):
-            inline_kb.insert(InlineKeyboardButton(
-                value,
-                callback_data=calendar_callback.new("SET-YEAR", value, -1, -1)
+            inline_row.append(InlineKeyboardButton(
+                text=value,
+                callback_data=build_data("SET-YEAR", value, -1, -1)
             ))
         # nav buttons
-        inline_kb.row()
-        inline_kb.insert(InlineKeyboardButton(
-            '<<',
-            callback_data=calendar_callback.new("PREV-YEARS", year, -1, -1)
+        inline_kb.append(inline_row)
+        inline_row = []
+        inline_row.append(InlineKeyboardButton(
+            text='<<',
+            callback_data=build_data("PREV-YEARS", year, -1, -1)
         ))
-        inline_kb.insert(InlineKeyboardButton(
-            '>>',
-            callback_data=calendar_callback.new("NEXT-YEARS", year, -1, -1)
+        inline_row.append(InlineKeyboardButton(
+            text='>>',
+            callback_data=build_data("NEXT-YEARS", year, -1, -1)
         ))
+        inline_kb.append(inline_row)
 
-        return inline_kb
+        return InlineKeyboardMarkup(row_width=5, inline_keyboard=inline_kb)
 
     async def _get_month_kb(self, year: int):
-        inline_kb = InlineKeyboardMarkup(row_width=6)
+        inline_kb = []
         # first row with year button
-        inline_kb.row()
-        inline_kb.insert(InlineKeyboardButton(" ", callback_data=ignore_callback))
-        inline_kb.insert(InlineKeyboardButton(
-            year,
-            callback_data=calendar_callback.new("START", year, -1, -1)
+        inline_row = []
+        inline_row.append(InlineKeyboardButton(text=" ", callback_data=ignore_callback))
+        inline_row.append(InlineKeyboardButton(
+            text=year,
+            callback_data=build_data("START", year, -1, -1)
         ))
-        inline_kb.insert(InlineKeyboardButton(" ", callback_data=ignore_callback))
+        inline_row.append(InlineKeyboardButton(text=" ", callback_data=ignore_callback))
         # two rows with 6 months buttons
-        inline_kb.row()
+        inline_kb.append(inline_row)
+        inline_row = []
         for month in self.months[0:6]:
-            inline_kb.insert(InlineKeyboardButton(
-                month,
-                callback_data=calendar_callback.new("SET-MONTH", year, self.months.index(month) + 1, -1)
+            inline_row.append(InlineKeyboardButton(
+                text=month,
+                callback_data=build_data("SET-MONTH", year, self.months.index(month) + 1, -1)
             ))
-        inline_kb.row()
+        inline_kb.append(inline_row)
+        inline_row = []
         for month in self.months[6:12]:
-            inline_kb.insert(InlineKeyboardButton(
-                month,
-                callback_data=calendar_callback.new("SET-MONTH", year, self.months.index(month) + 1, -1)
+            inline_row.append(InlineKeyboardButton(
+                text=month,
+                callback_data=build_data("SET-MONTH", year, self.months.index(month) + 1, -1)
             ))
-        return inline_kb
+        inline_kb.append(inline_row)
+        return InlineKeyboardMarkup(row_width=6, inline_keyboard=inline_kb)
 
     async def _get_days_kb(self, year: int, month: int):
-        inline_kb = InlineKeyboardMarkup(row_width=7)
-        inline_kb.row()
-        inline_kb.insert(InlineKeyboardButton(
-            year,
-            callback_data=calendar_callback.new("START", year, -1, -1)
+        inline_kb = []
+        inline_row = []
+        inline_row.append(InlineKeyboardButton(
+            text=year,
+            callback_data=build_data("START", year, -1, -1)
         ))
-        inline_kb.insert(InlineKeyboardButton(
-            self.months[month - 1],
-            callback_data=calendar_callback.new("SET-YEAR", year, -1, -1)
+        inline_row.append(InlineKeyboardButton(
+            text=self.months[month - 1],
+            callback_data=build_data("SET-YEAR", year, -1, -1)
         ))
-        inline_kb.row()
+        inline_kb.append(inline_row)
+        inline_row = []
         for day in ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]:
-            inline_kb.insert(InlineKeyboardButton(day, callback_data=ignore_callback))
+            inline_row.append(InlineKeyboardButton(text=day, callback_data=ignore_callback))
+        inline_kb.append(inline_row)
 
         month_calendar = calendar.monthcalendar(year, month)
         for week in month_calendar:
-            inline_kb.row()
+            inline_row = []
             for day in week:
                 if (day == 0):
-                    inline_kb.insert(InlineKeyboardButton(" ", callback_data=ignore_callback))
+                    inline_row.append(InlineKeyboardButton(text=" ", callback_data=ignore_callback))
                     continue
-                inline_kb.insert(InlineKeyboardButton(
-                    str(day), callback_data=calendar_callback.new("SET-DAY", year, month, day)
+                inline_row.append(InlineKeyboardButton(
+                    text=str(day), callback_data=build_data("SET-DAY", year, month, day)
                 ))
-        return inline_kb
+            inline_kb.append(inline_row)
+        return InlineKeyboardMarkup(row_width=7, inline_keyboard=inline_kb)
 
-    async def process_selection(self, query: CallbackQuery, data: CallbackData) -> tuple:
+    async def process_selection(self, query: CallbackQuery) -> tuple:
         return_data = (False, None)
+        data = process_data(query.data)
         if data['act'] == "IGNORE":
             await query.answer(cache_time=60)
         if data['act'] == "SET-YEAR":
-            await query.message.edit_reply_markup(await self._get_month_kb(int(data['year'])))
+            await query.message.edit_reply_markup(reply_markup=await self._get_month_kb(int(data['year'])))
         if data['act'] == "PREV-YEARS":
             new_year = int(data['year']) - 5
-            await query.message.edit_reply_markup(await self.start_calendar(new_year))
+            await query.message.edit_reply_markup(reply_markup=await self.start_calendar(new_year))
         if data['act'] == "NEXT-YEARS":
             new_year = int(data['year']) + 5
-            await query.message.edit_reply_markup(await self.start_calendar(new_year))
+            await query.message.edit_reply_markup(reply_markup=await self.start_calendar(new_year))
         if data['act'] == "START":
-            await query.message.edit_reply_markup(await self.start_calendar(int(data['year'])))
+            await query.message.edit_reply_markup(reply_markup=await self.start_calendar(int(data['year'])))
         if data['act'] == "SET-MONTH":
-            await query.message.edit_reply_markup(await self._get_days_kb(int(data['year']), int(data['month'])))
+            await query.message.edit_reply_markup(reply_markup=await self._get_days_kb(int(data['year']), int(data['month'])))
         if data['act'] == "SET-DAY":
             await query.message.delete_reply_markup()   # removing inline keyboard
             return_data = True, datetime(int(data['year']), int(data['month']), int(data['day']))
