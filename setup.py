3008371dfd7f2dd372cbf381b2d3f7447ef121ef
barabum0 <r.m.poltorabatko@gmail.com>
2023-06-17 13:41:29 +0300

    updated setup copyrights etc

--- setup.py
+++ setup.py
@@ -4,19 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name='aiogram_calendar',
-  packages=['aiogram_calendar'],
-  version='0.1.1',
+  name='aiogram3_calendar',
+  packages=['aiogram3_calendar'],
+  version='0.2',
   license='MIT',
   description='Simple Inline Calendar & Date Selection tool for Aiogram Telegram bots',
   long_description=long_description,
-  author='Andrew Nikolabay',
+  author='Roman Poltorabatko (original by Andrew Nikolabay)',
   author_email='',
-  url='https://github.com/noXplode/aiogram_calendar',
-  download_url='https://github.com/noXplode/aiogram_calendar/archive/refs/tags/0.1.1.tar.gz',
+  url='https://github.com/barabum0/aiogram_calendar',
   keywords=['Aiogram', 'Telegram', 'Bots', 'Calendar'],
   install_requires=[
-          'aiogram',
+          'aiogram~=3.0.0b7',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',
@@ -28,5 +27,8 @@ setup(
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12'
   ],
 )
