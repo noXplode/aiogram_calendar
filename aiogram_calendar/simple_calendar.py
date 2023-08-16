b89cfeeef23aa37aac2543d5950a91377fcf4816
svan5003 <57659819+svan5003@users.noreply.github.com>
2023-08-16 16:25:07 +0300

    Update simple_calendar.py
    necessary inline_message_id added

--- aiogram_calendar/simple_calendar.py
+++ aiogram_calendar/simple_calendar.py
@@ -89,18 +89,18 @@ class SimpleCalendar:
         # user navigates to previous year, editing message with new calendar
         if data['act'] == "PREV-YEAR":
             prev_date = datetime(int(data['year']) - 1, int(data['month']), 1)
-            await query.message.edit_reply_markup(await self.start_calendar(int(prev_date.year), int(prev_date.month)))
+            await query.message.edit_reply_markup(query.inline_message_id, await self.start_calendar(int(prev_date.year), int(prev_date.month)))
         # user navigates to next year, editing message with new calendar
         if data['act'] == "NEXT-YEAR":
             next_date = datetime(int(data['year']) + 1, int(data['month']), 1)
-            await query.message.edit_reply_markup(await self.start_calendar(int(next_date.year), int(next_date.month)))
+            await query.message.edit_reply_markup(query.inline_message_id, await self.start_calendar(int(next_date.year), int(next_date.month)))
         # user navigates to previous month, editing message with new calendar
         if data['act'] == "PREV-MONTH":
             prev_date = temp_date - timedelta(days=1)
-            await query.message.edit_reply_markup(await self.start_calendar(int(prev_date.year), int(prev_date.month)))
+            await query.message.edit_reply_markup(query.inline_message_id, await self.start_calendar(int(prev_date.year), int(prev_date.month)))
         # user navigates to next month, editing message with new calendar
         if data['act'] == "NEXT-MONTH":
             next_date = temp_date + timedelta(days=31)
-            await query.message.edit_reply_markup(await self.start_calendar(int(next_date.year), int(next_date.month)))
+            await query.message.edit_reply_markup(query.inline_message_id, await self.start_calendar(int(next_date.year), int(next_date.month)))
         # at some point user clicks DAY button, returning date
         return return_data
